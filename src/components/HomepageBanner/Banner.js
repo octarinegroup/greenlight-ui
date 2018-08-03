d4b36dee277ba3dcf85df5a5d887b1141847a49f
u-ashish <ashish.uppala.93@gmail.com>
2018-08-02 21:50:30 -0400

    added homecontainer, navbar, banner

---
+++ src/components/HomepageBanner/Banner.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import './Banner.css'
+
+export const Banner = (props) => (
+  <div className="banner-wrapper">
+    <div className="banner-inner-wrapper">
+      <div className="banner-text-wrapper">
+        <div className="banner-header">
+          Validate new ideas by seamlessly engageing your target market
+        </div>
+        <div className="banner-subheader">
+          Create surveys | Reach your audience | Analyze Results
+        </div>
+      </div>
+      <div className="banner-button">
+        <button>Get Started</button>
+      </div>
+    </div>
+    <div className="banner-image">
+      <img src="/assets/images/banner.png" alt="banner" />
+    </div>
+  </div>
+)
