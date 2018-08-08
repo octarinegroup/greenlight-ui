72df8ce9a569cb20244c3c39a01fa2881225351a
Derek Warren <derekwarren@Dereks-MacBook-Pro-2.local>
2018-08-07 21:38:31 -0400

    created callout section

---
+++ src/components/Callout/Callout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './Callout.css'
+
+export const Callout = props => (
+  <div className="callout-wrapper">
+    <span>We make it easy for you to get feedback on ideas from people that matter.</span>
+  </div>
+)
